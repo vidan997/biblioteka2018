0e1b39504eb2eb11932c22208289c73c13df941a
vidan997 <milojevic27116@fon.bg.ac.rs>
2018-03-02 11:15:48 +0100

    Refaktorisana klasa Knjiga, uvedena klasa Autor

--- Biblioteka/src/Knjiga.java
+++ Biblioteka/src/Knjiga.java
@@ -1,4 +1,39 @@
 
 public class Knjiga {
 	
+	private String naslov, isbn, izdavac, autori;
+	private int izdanje;
+	
+	
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getIzdavac() {
+		return izdavac;
+	}
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+	public String getAutori() {
+		return autori;
+	}
+	public void setAutori(String autori) {
+		this.autori = autori;
+	}
+	public int getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	
 }
