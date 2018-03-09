62c989aa189048366ab97309155363c2dc7a67d3
vidan997 <milojevic27116@fon.bg.ac.rs>
2018-03-09 10:40:57 +0100

    Implementirana klasa Biblioteka

--- Biblioteka/src/Biblioteka/Biblioteka.java
+++ Biblioteka/src/Biblioteka/Biblioteka.java
@@ -1,5 +1,38 @@
 package Biblioteka;
 
-public class Biblioteka {
+import java.util.LinkedList;
+
+import Biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs{
+	
+	LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
+	
+	public void dodajKnjigu(Knjiga k) {
+		knjige.add(k);
+	}
+
+	public void obrisiKnjigu(Knjiga k) {
+		knjige.remove(k);		
+	}
+
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		LinkedList<Knjiga>	novaLista = new LinkedList<Knjiga>();
+		
+		for(int i = 0; i < knjige.size() ; i++) {
+			if(knjige.get(i).getNaslov().contains(naslov)) {
+				novaLista.add(knjige.get(i));
+			}
+		}
+		
+		return novaLista;
+	}
+	
+	
 
 }
